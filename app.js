d2dd29b6538a0ee4a1fde843d7384865078aba48
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2022-11-18 15:11:00 +0100

    1.0

---
+++ app.js
@@ -0,0 +1,98 @@
+// Country radio
+
+
+// dutchradio's
+let pnl = document.querySelector(".honderd");
+
+
+function nlp() {
+    if (pnl.classList.contains("active")) {
+        pnl.classList.remove("active")
+    } else {
+        pnl.classList.add("active")
+    }
+}
+
+let fomix = document.querySelector(".fomix");
+
+function femix() {
+    if (fomix.classList.contains("active")) {
+        fomix.classList.remove("active")
+    } else {
+        fomix.classList.add("active")
+    }
+}
+let tien = document.querySelector(".radiotien");
+
+function radiotien() {
+    if (tien.classList.contains("active")) {
+        tien.classList.remove("active")
+    } else {
+        tien.classList.add("active")
+    }
+}
+let veronic = document.querySelector(".veronica");
+
+function veronica() {
+    if (veronic.classList.contains("active")) {
+        veronic.classList.remove("active")
+    } else {
+        veronic.classList.add("active")
+    }
+}
+let sky = document.querySelector(".skyradio");
+
+function skyradio() {
+    if (sky.classList.contains("active")) {
+        sky.classList.remove("active")
+    } else {
+        sky.classList.add("active")
+    }
+}
+let np = document.querySelector(".npo");
+
+function npo() {
+    if (np.classList.contains("active")) {
+        np.classList.remove("active")
+    } else {
+        np.classList.add("active")
+    }
+}
+let gpp = document.querySelector(".gpr");
+
+function gpr() {
+    if (gpp.classList.contains("active")) {
+        gpp.classList.remove("active")
+    } else {
+        gpp.classList.add("active")
+    }
+}
+
+let dutcht = document.querySelector(".dutch");
+
+function dutch() {
+    if (dutcht.classList.contains("active")) {
+        dutcht.classList.remove("active")
+    } else {
+        dutcht.classList.add("active")
+    }
+}
+// usradio
+let ust = document.querySelector(".us");
+
+function us() {
+    if (ust.classList.contains("active")) {
+        ust.classList.remove("active")
+    } else {
+        ust.classList.add("active")
+    }
+}
+let mrct = document.querySelector(".mrc");
+
+function mrc() {
+    if (mrct.classList.contains("active")) {
+        mrct.classList.remove("active")
+    } else {
+        mrct.classList.add("active")
+    }
+}
